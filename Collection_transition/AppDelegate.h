1f0db1d76790baeda45960c4ba8ad112f964ec3d
SingYi <23323231@qq.com>
2016-06-23 17:59:40 +0800

    Initial Commit

---
+++ Collection_transition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Collection_transition
+//
+//  Created by 石燚 on 16/6/23.
+//  Copyright © 2016年 SingYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Collection_transition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Collection_transition
+//
+//  Created by 石燚 on 16/6/23.
+//  Copyright © 2016年 SingYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
